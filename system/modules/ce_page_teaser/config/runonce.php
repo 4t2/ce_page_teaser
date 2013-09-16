8e03706f6db3ce7c8f83fe74054de6ae754e454c
Mario Müller <mario@lingo4u.de>
2013-09-16 15:57:51 +0200

    +runone.php

---
+++ system/modules/ce_page_teaser/config/runonce.php
@@ -0,0 +1,26 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+class TeaserRunonceJob extends Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->import('Database');
+	}
+
+	public function run()
+	{
+		// nur ab Contao 2.9
+		if (version_compare(VERSION, '2.8', '>'))
+		{
+			if ($this->Database->fieldExists('page_teaser_text', 'tl_content'))
+			{
+				$this->Database->execute("UPDATE `tl_content` SET `text`=`page_teaser_text` WHERE `type`='page_teaser'");
+				$this->Database->execute("ALTER TABLE `tl_content` DROP COLUMN `page_teaser_text`");
+			}
+		}
+	}
+}
+
+$objTeaserRunonceJob = new TeaserRunonceJob();
+$objTeaserRunonceJob->run();
